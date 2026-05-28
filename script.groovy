b1bfd145dafba07eb7865324ae513741b444da39
unknown <omotolaadebulu@gmail.com>
2026-05-28 19:16:12 +0100

    made some changes to groovy script

--- script.groovy
+++ script.groovy
@@ -1,19 +1,19 @@
 def buildJar() {
     echo "building the application..."
-    sh "mvn clean package"
+    // sh "mvn clean package"
 } 
 
 def createImage() {
     echo "creating the docker image..."
-    sh "docker build -t sunesis003/app-jenkins:5.0 ."
+    // sh "docker build -t sunesis003/app-jenkins:5.0 ."
 } 
 
 def publishImage() {
     echo "publishing to docker hub repo..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push sunesis003/app-jenkins:5.0"
-    }
+    // withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    //     sh "echo $PASS | docker login -u $USER --password-stdin"
+    //     sh "docker push sunesis003/app-jenkins:5.0"
+    // }
 } 
 
 def deployApp() {
