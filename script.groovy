39d1ca58a8c033648438b5486d06939ead1ad3eb
omotola <omotolaadebulu@gmail.com>
2026-06-02 15:39:11 +0100

    added logic for deploy to ec2 instance

--- script.groovy
+++ script.groovy
@@ -1,37 +1,41 @@
 def increaseVersion(){
-    echo "incrementing app version..."
-    sh "mvn build-helper:parse-version versions:set \
-        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
-        versions:commit"
-    def matcher = readFile("pom.xml") =~ "<version>(.+)</version>"
-    def version = matcher[0][1]
-    env.IMAGE_TAG = "$version-$BUILD_NUMBER"
+    // echo "incrementing app version..."
+    // sh "mvn build-helper:parse-version versions:set \
+    //     -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+    //     versions:commit"
+    // def matcher = readFile("pom.xml") =~ "<version>(.+)</version>"
+    // def version = matcher[0][1]
+    // env.IMAGE_TAG = "$version-$BUILD_NUMBER"
 }
 
 def buildJar() {
     echo "building and testing the java application..."
-    echo "executing pipeline for $BRANCH_NAME branch"
-    sh "mvn clean package"
+    // echo "executing pipeline for $BRANCH_NAME branch"
+    // sh "mvn clean package"
 } 
 
 
 def createImage() {
     echo "creating the docker image..."
-    sh "docker build -t sunesis003/app-jenkins:${IMAGE_TAG} ."
+    // sh "docker build -t sunesis003/app-jenkins:${IMAGE_TAG} ."
 } 
 
 
 def publishImage() {
     echo "publishing to docker image to docker hub repo..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push sunesis003/app-jenkins:${IMAGE_TAG}"
-    }
+    // withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    //     sh "echo $PASS | docker login -u $USER --password-stdin"
+    //     sh "docker push sunesis003/app-jenkins:${IMAGE_TAG}"
+    // }
 } 
 
 
 def deployApp() {
     echo "deploying the application to AWS EC2 Instance..."
+    def dockerCmd = "docker run -p 3080:3080 -d sunesis003/app-jenkins:jsl-1.0"
+    sshagent(['ec2-server-key']) {
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@56.228.36.89 ${dockerCmd}"
+    }
 } 
 
 
