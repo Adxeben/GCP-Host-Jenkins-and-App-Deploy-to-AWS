0b54ded57123390cef5d9b43f57668850bf84db1
omotola <omotolaadebulu@gmail.com>
2026-06-19 09:16:25 +0100

    updated server ip

--- script.groovy
+++ script.groovy
@@ -33,7 +33,7 @@ def increaseVersion(){
 def deployApp() {
     echo "deploying the application to AWS EC2 Instance..."
     
-    def ec2Instance = "ubuntu@13.49.183.118"
+    def ec2Instance = "ubuntu@13.48.49.115"
     def shellCmd = "bash ./server-cmds.sh ${IMAGE_NAME}"
 
     sshagent(['aws-ubuntu-server-key']) {
